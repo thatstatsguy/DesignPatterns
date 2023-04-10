9f26a232e314091774caa02ffe2d5cbaf211e560
ThatStatsGuy <chris.dunderdale.1995@gmail.com>
2023-04-10 12:03:28 +0200

    don't add controllers to services if not needed

--- Hub and Spoke Pattern/Part1/CurrencyDisplay/Program.cs
+++ Hub and Spoke Pattern/Part1/CurrencyDisplay/Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddControllers();
 
 var app = builder.Build();
 
