ad82bb657882ff0f197b8235da1bffef27f707d5
ThatStatsGuy <chris.dunderdale.1995@gmail.com>
2023-04-10 17:01:00 +0200

    Start part 2

---
+++ Hub and Spoke Pattern/Part2/CurrencyDisplay/DTO/PriceUpdate.cs
@@ -0,0 +1,9 @@
+namespace CurrencyDisplay.DTO;
+
+/// <summary>
+/// Represents a price update for a given currency conversion e.g. ZAR -> USD 
+/// </summary>
+/// <param name="FromCurrency">The currency being exchanged e.g. ZAR</param>
+/// <param name="ToCurrency">The currency being converted to e.g. USD</param>
+/// <param name="CurrencyConversionRate">The amount of the target currency received for each "unit" of the original currency.</param>
+public record PriceUpdate(string FromCurrency, string ToCurrency, double CurrencyConversionRate);
