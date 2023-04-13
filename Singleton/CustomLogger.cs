c2c696b52b85237ba112e1c4bf1844a4b1ee8964
ThatStatsGuy <chris.dunderdale.1995@gmail.com>
2023-04-13 17:24:59 +0200

    make the logger thread safe

--- Singleton/CustomLogger.cs
+++ Singleton/CustomLogger.cs
@@ -2,21 +2,11 @@ namespace Singleton;
 
 public class CustomLogger
 {
+    // lazy implementation ensures that this is thread safe
+    private static readonly Lazy<CustomLogger> _logger = new Lazy<CustomLogger>(() => new CustomLogger());
+    
+    public static CustomLogger Instance => _logger.Value;
 
-    private static CustomLogger? _logger;
-
-    public static CustomLogger Instance
-    {
-        get
-        {
-            if (_logger is null)
-            {
-                _logger = new CustomLogger();
-            }
-
-            return _logger;
-        }
-    }
     //only available to classes that inherit from this class
     protected CustomLogger()
     {
