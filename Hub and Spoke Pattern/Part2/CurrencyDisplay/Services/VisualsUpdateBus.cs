7e61d5675167d5c96d62a1b4c7d0ae67a3ebfcc5
ThatStatsGuy <chris.dunderdale.1995@gmail.com>
2023-04-16 11:50:59 +0200

    Fix indentation

--- Hub and Spoke Pattern/Part2/CurrencyDisplay/Services/VisualsUpdateBus.cs
+++ Hub and Spoke Pattern/Part2/CurrencyDisplay/Services/VisualsUpdateBus.cs
@@ -9,71 +9,71 @@ namespace CurrencyDisplay.Services;
 public class VisualsUpdateBus : IUiBus
 {
     private bool _disposed;
-        private readonly ISet<IListener> _listeners = new HashSet<IListener>();
-        private readonly ILogger<VisualsUpdateBus> _logger;
+    private readonly ISet<IListener> _listeners = new HashSet<IListener>();
+    private readonly ILogger<VisualsUpdateBus> _logger;
 
-        public VisualsUpdateBus(ILogger<VisualsUpdateBus> logger)
-        {
-            _logger = logger;
-        }
+    public VisualsUpdateBus(ILogger<VisualsUpdateBus> logger)
+    {
+        _logger = logger;
+    }
 
-        public void Register(IListener listener)
-        {
-            if (listener == null) return;
-            _logger.LogDebug("Listener registering: {0}", GetObjectIdentifier(listener));
+    public void Register(IListener listener)
+    {
+        if (listener == null) return;
+        _logger.LogDebug("Listener registering: {0}", GetObjectIdentifier(listener));
 
-            _listeners.Add(listener);
-        }
+        _listeners.Add(listener);
+    }
 
-        public void UnRegister(IListener listener)
-        {
-            _logger.LogDebug("Listener unregistering: {0}", GetObjectIdentifier(listener));
-            _listeners.Remove(listener);
-        }
+    public void UnRegister(IListener listener)
+    {
+        _logger.LogDebug("Listener unregistering: {0}", GetObjectIdentifier(listener));
+        _listeners.Remove(listener);
+    }
 
-        public IListener<T>[] GetListeners<T>() where T : IUiBusEvent
-        {
-            IEnumerable<IListener> listeners = _listeners.Where(c => c is IListener<T>);
-            IEnumerable<IListener<T>> enumerable = listeners.Select(listener => (IListener<T>)listener);
-            return enumerable.ToArray();
-        }
+    public IListener<T>[] GetListeners<T>() where T : IUiBusEvent
+    {
+        IEnumerable<IListener> listeners = _listeners.Where(c => c is IListener<T>);
+        IEnumerable<IListener<T>> enumerable = listeners.Select(listener => (IListener<T>)listener);
+        return enumerable.ToArray();
+    }
 
-        public void Notify<T>(T notification) where T : IUiBusEvent
+    public void Notify<T>(T notification) where T : IUiBusEvent
+    {
+        IListener<T>[] listeners = GetListeners<T>();
+        string message = string.Format("Notifying {0} of {1} listeners with {2} on bus {3}",
+            listeners.Length, _listeners.Count, notification.GetType().Name, GetHashCode());
+        
+        _logger.LogInformation(message);
+        foreach (IListener<T> listener in listeners)
         {
-            IListener<T>[] listeners = GetListeners<T>();
-            string message = string.Format("Notifying {0} of {1} listeners with {2} on bus {3}",
-                listeners.Length, _listeners.Count, notification.GetType().Name, GetHashCode());
-            
-            _logger.LogInformation(message);
-            foreach (IListener<T> listener in listeners)
-            {
-                _logger.LogInformation("Notifying: {0} with {1}", 
-                    GetObjectIdentifier(listener), notification.GetType().Name);
-                listener.Handle(notification);
-            }
+            _logger.LogInformation("Notifying: {0} with {1}", 
+                GetObjectIdentifier(listener), notification.GetType().Name);
+            listener.Handle(notification);
         }
+    }
 
-        public void UnRegisterAll()
-        {
-            _listeners.Clear();
-        }
+    public void UnRegisterAll()
+    {
+        _listeners.Clear();
+    }
 
-        private static string GetObjectIdentifier(object obj)
-        {
-            return $"{obj.GetType().Name}-{obj.GetHashCode()}";
-        }
+    private static string GetObjectIdentifier(object obj)
+    {
+        return $"{obj.GetType().Name}-{obj.GetHashCode()}";
+    }
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (_disposed) { return; }
-            if (disposing) {
-                UnRegisterAll();
-            }
-            _disposed = true;
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed) { return; }
+        if (disposing) {
+            UnRegisterAll();
         }
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+        _disposed = true;
+    }
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
 }
