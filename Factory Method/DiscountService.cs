af6afc51833f1e655a6d69723cfe178162c6d821
ThatStatsGuy <chris.dunderdale.1995@gmail.com>
2023-04-18 09:39:24 +0200

    fill in factory pattern

--- Factory Method/DiscountService.cs
+++ Factory Method/DiscountService.cs
@@ -1,6 +1,93 @@
 namespace Factory_Method_Pattern;
 
-public class DiscountService
+
+// alternative to an interface - the thing that needs to be produced
+// also known in the pattern structure as the "Product"
+public abstract class DiscountService
 {
-    
+    public abstract int DiscountPercentage { get; }
+    public override string ToString()
+    {
+        return GetType().Name;
+    }
+}
+
+//know as the concrete product in pattern structure
+public class CountryDiscountService : DiscountService
+{
+    private readonly string _countryIdentifier;
+
+    public CountryDiscountService(string countryIdentifier)
+    {
+        _countryIdentifier = countryIdentifier;
+    }
+
+    public override int DiscountPercentage
+    {
+        get
+        {
+            switch (_countryIdentifier)
+            {
+                case "ZA" :
+                    return 20;
+                default: return 10;
+            }
+        }
+    }
+}
+
+//know as the concrete product in pattern structure
+public class CodeDiscountService : DiscountService
+{
+    private readonly Guid _code;
+
+    public CodeDiscountService(Guid code)
+    {
+        _code = code;
+    }
+
+    public override int DiscountPercentage => 15;
+}
+
+
+// used to specify a method which is used to create a "product"
+// known as a "creator" in pattern structure
+public abstract class DiscountFactory
+{
+    public abstract DiscountService CreateDiscountService();
+}
+
+// implements methods from the creator by overriding the factory method
+// known as a "concrete creator"
+public class CodeDiscountFactory : DiscountFactory
+{
+    private readonly Guid _code;
+
+    public CodeDiscountFactory(Guid code)
+    {
+        _code = code;
+    }
+
+    public override DiscountService CreateDiscountService()
+    {
+        return new CodeDiscountService(_code);
+    }
+}
+
+/// <summary>
+/// Concrete creator
+/// </summary>
+public class CountryDiscountFactory : DiscountFactory
+{
+    private readonly string _countryIdentifier;
+
+    public CountryDiscountFactory(string countryIdentifier)
+    {
+        _countryIdentifier = countryIdentifier;
+    }
+
+    public override DiscountService CreateDiscountService()
+    {
+        return new CountryDiscountService(_countryIdentifier);
+    }
 }
--- Factory Method/Program.cs
+++ Factory Method/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+using Factory_Method_Pattern;
+
 Console.WriteLine("Hello, World!");
+var factories  = new List<DiscountFactory>
+{
+    new CodeDiscountFactory(Guid.NewGuid()),
+    new CountryDiscountFactory("ZA")
+};
+
+foreach (var factory in factories)
+{
+    var discountingService = factory.CreateDiscountService();
+    Console.WriteLine($"Discount of {discountingService}: {discountingService.DiscountPercentage}");
+}
